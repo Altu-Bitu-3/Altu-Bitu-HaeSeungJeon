6a58b632f3c232a52c6251ffca1d91c45fa14041
JeonHaeseung <414catherine@gmail.com>
2022-09-12 18:35:24 +0900

    [p3] 코드 리뷰 반영
    10757 문제의 p3 코드 리뷰를 반영하여 추가 수정했습니다!

--- [0906] 맵과 셋 과제/0906 백준 10757.cpp
+++ [0906] 맵과 셋 과제/0906 백준 10757.cpp
@@ -2,18 +2,10 @@
 
 using namespace std;
 
-int main() {
-    string A, B; //숫자가 크기 때문에 문자열로 저장
-    string ans; //답을 저장할 문자열
-    int A_size, B_size; //A 문자열과 B 문자열의 길이
-    int ans_size=0; //답이 되는 문자열의 길이
+string add(int &A_size, int &B_size, string &A, string &B){
+    string ans;
     int round_up = 0; //계산하는 과정에서 올림수
     
-    cin >> A >> B;
-    A_size = A.length();
-    B_size = B.length();
-    
-    //두 문자열 중 하나라도 남아있으면 계산을 진행
     while(A_size > 0 || B_size > 0){ 
         int sum = 0;
         if(A_size > 0){ //A 문자열에 숫자가 남아있으면 더하기
@@ -27,12 +19,24 @@ int main() {
         round_up = sum/10; 
         sum = sum % 10;
         ans += sum + '0';   //ans 문자열 뒤에 붙이기
-        ans_size++;
     }
     if(round_up >0){ //가장 높은 자릿수 계산까지 했는데 올림수가 남아있다면
         ans += round_up + '0'; //ans에 반영
     }
+    return ans;
+}
+
+int main() {
+    string A, B; //숫자가 크기 때문에 문자열로 저장
+    string ans; //답을 저장할 문자열
+    int A_size, B_size; //A 문자열과 B 문자열의 길이
     
+    cin >> A >> B;
+    A_size = A.length();
+    B_size = B.length();
+    
+    ans = add(A_size, B_size, A, B);
+
     for(int i=ans.length()-1;i>=0;i--) { //역순으로 출력
         cout << ans[i];
     }
