3755b4c105b3bdbb0af772f7372974fe83706ebd
JeonHaeseung <414catherine@gmail.com>
2022-09-24 17:18:44 +0900

    [p2, p3] 코드 리뷰 반영
    17299 문제의 p2, p3 코드 리뷰를 반영하여 추가 수정했습니다!

--- [0909] 스텍, 큐, 덱 과제/0909 백준 17299.cpp
+++ [0909] 스텍, 큐, 덱 과제/0909 백준 17299.cpp
@@ -2,8 +2,6 @@
 #include <stack>
 #include <map>
 
-int F[1000001], ans[1000001], num[1000001]; //인덱스가 숫자 a가 되고, 요소가 F(a) 즉 우선순위가 되는 배열 F
-
 using namespace std;
 
 /*  오른쪽에서 왼쪽으로 s[i] 검사:
@@ -13,23 +11,23 @@ using namespace std;
     3. 스텍이 empty 상태면 push한다.
 */
 
-void ngf(int n){
+void ngf(int n, int frequency[], int num[], int ans[]){
     stack<int> st;
     
     for (int i = n - 1; i >= 0; i--) {
 		while (!st.empty()) {
 		    //우선순위가 스텍 top의 우선순위보다 크거나 같으면 스텍 top을 pop
-			if (F[num[i]] >= F[num[st.top()]]){
+			if (frequency[num[i]] >= frequency[st.top()]){
 			    st.pop();
 			}
 			else break;
 		}
-		ans[i] = -1;
+		ans[i] = -1; //오등큰수가 없으면 -1로 설정
 		//우선순위가 스텍 top의 우선순위보다 작으면 스텍에 push
 		if (!st.empty()){
-		    ans[i] = num[st.top()];
+		    ans[i] = st.top();
 		}
-		st.push(i); //스텍이 empty 상태면 push
+		st.push(num[i]); //스텍이 empty 상태면 push
 	}
 }
 
@@ -39,14 +37,18 @@ int main() {
     
     cin >> n;
     
+    const int MAX = 1e6+1;
+    
+    int frequency[MAX], num[MAX], ans[MAX]; //인덱스가 숫자 a가 되고, 요소가 F(a) 즉 우선순위가 되는 배열 frequency
+
     //F 값, 숫자 저장
     for(int i=0;i<n;i++){
         int a;
         cin >> a;
-        num[i] = a; F[num[i]]++; 
+        num[i] = a; frequency[num[i]]++; 
     }
     
-    ngf(n);
+    ngf(n, frequency, num, ans);
     
     for(int i=0;i<n;i++){
         cout << ans[i] << ' ';
